39b25f8251534c665da1a378f5c6fa91e2f17eb8
Aashutosh sharma <67682159+Aashutoshsharma002@users.noreply.github.com>
2021-05-10 10:47:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Change the text between the quotation marks and run your code to see your name animated!
+drawName('Hello World ! 😀😎');
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
